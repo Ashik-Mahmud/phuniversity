561bfba78b90148e3126434555c93768df7d88cb
ashikmahmud <ashikmamud187@gmail.com>
2024-11-06 06:19:25 +0600

    Add table of all courses

--- src/pages/dashboard/admin/courseManagement/offerCourse.tsx
+++ src/pages/dashboard/admin/courseManagement/offerCourse.tsx
@@ -1,7 +1,53 @@
+import { useGetAllCoursesQuery } from "@/redux/features/admin/courseManagementApi";
+import { ICourseItem } from "@/types/courses.types";
+import { LoadingOverlay, Table, Title } from "@mantine/core";
+import { BsTrash2 } from "react-icons/bs";
+
 type Props = {};
 
 const OfferCourses = (props: Props) => {
-  return <div>offerCourse</div>;
+  const { data, isLoading } = useGetAllCoursesQuery({});
+
+  const rows = data?.data?.map((element: ICourseItem) => (
+    <Table.Tr key={element._id}>
+      <Table.Td>{element.title}</Table.Td>
+      <Table.Td>{element.code}</Table.Td>
+      <Table.Td>{element.credits}</Table.Td>
+      <Table.Td>{element.prefix}</Table.Td>
+      <Table.Td>
+        <BsTrash2 />
+      </Table.Td>
+    </Table.Tr>
+  ));
+  if (isLoading) {
+    return (
+      <LoadingOverlay
+        visible={isLoading}
+        zIndex={1000}
+        overlayProps={{ radius: "sm", blur: 2 }}
+      />
+    );
+  }
+  return (
+    <div className="p-10">
+      <Title order={2} size={30} mb={30}>
+        Courses list
+      </Title>
+
+      <Table stickyHeader stickyHeaderOffset={0}>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>Course Name</Table.Th>
+            <Table.Th>Code</Table.Th>
+            <Table.Th> Credits</Table.Th>
+            <Table.Th>Prefix</Table.Th>
+            <Table.Th>Action</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>{rows}</Table.Tbody>
+      </Table>
+    </div>
+  );
 };
 
 export default OfferCourses;
